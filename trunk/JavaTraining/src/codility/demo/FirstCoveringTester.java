1180ee745f8264b721cd814cf3bc565d57fd41e9
jsalatan@gmail.com <jsalatan@gmail.com@bcfe4fa1-e7eb-a4ed-8328-02bcd61daf47>
2011-07-05 09:40:19 +0000

    working copy. Added FirstCovering excercise.

---
+++ trunk/JavaTraining/src/codility/demo/FirstCoveringTester.java
@@ -0,0 +1,15 @@
+package codility.demo;
+
+
+public class FirstCoveringTester {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int[] A = { 2, 2, 1, 0, 1 };
+		FirstCovering fc = new FirstCovering();
+		System.out.println(fc.ps(A));
+	}
+}
