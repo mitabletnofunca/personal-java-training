ec69d6096c7419496f6850606beb5c8fb7753eaf
jsalatan@gmail.com <jsalatan@gmail.com@bcfe4fa1-e7eb-a4ed-8328-02bcd61daf47>
2011-07-10 23:05:45 +0000

    Equilibrium Index and Equalization steps commit.

---
+++ JavaTraining/src/codility/demo/EquilibriumIndexTester.java
@@ -0,0 +1,15 @@
+package codility.demo;
+
+
+public class EquilibriumIndexTester {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int[] A = { -7, 1, 5, 2, -4, 3, 0 };
+		EquilibriumIndex instance = new EquilibriumIndex();
+		System.out.println(instance.equi(A));
+	}
+}
