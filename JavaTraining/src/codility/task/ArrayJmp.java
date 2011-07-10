0fddbb85354fb425d77502ff33f2c29cb4e9b55f
jsalatan@gmail.com <jsalatan@gmail.com@bcfe4fa1-e7eb-a4ed-8328-02bcd61daf47>
2011-07-10 11:10:24 +0000

    Arrray Jmp commit.

---
+++ JavaTraining/src/codility/task/ArrayJmp.java
@@ -0,0 +1,19 @@
+package codility.task;
+
+
+class ArrayJmp {
+  
+  int arrayJmp ( int[] A ) {
+
+	  int cnt = 0, indx = 0;
+	  while (indx < A.length) {
+		  if (indx != A[indx]) {
+			  indx = A[indx] + indx;
+			  cnt++;
+		  } else {
+			  return -1;
+		  }
+	  }
+	  return cnt;
+  }
+}
---
+++ JavaTraining/src/codility/task/ArrayJmpTester.java
@@ -0,0 +1,15 @@
+package codility.task;
+
+
+public class ArrayJmpTester {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int[] A = { 2, 3, 1, 1, 3 };
+		ArrayJmp instance = new ArrayJmp();
+		System.out.println(instance.arrayJmp(A));
+	}
+}
