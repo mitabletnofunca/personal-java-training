4913c0f36b1fb9f305e7c6da6093ba777e1a9dfb
jsalatan@gmail.com <jsalatan@gmail.com@bcfe4fa1-e7eb-a4ed-8328-02bcd61daf47>
2011-07-10 10:08:56 +0000

    First Covering Prefix v1.0. Result = 96%.

--- JavaTraining/src/codility/demo/FirstCovering.java
+++ JavaTraining/src/codility/demo/FirstCovering.java
@@ -7,26 +7,20 @@ import java.util.Iterator;
 
 class FirstCovering {
     int ps ( int[] A ) {
-    	// write your code here
-    	
     	ArrayList<Integer> arrA = new ArrayList<Integer>(A.length);
     	for (int i = 0; i < A.length; i++) {
     		arrA.add(A[i]);
     	}
     	
-    	ArrayList<Integer> sortedA = new ArrayList<Integer>(arrA);
     	HashSet<Integer> hashSet = new HashSet<Integer>(arrA);
-    	
     	Iterator<Integer> iter = hashSet.iterator();
 
-    	int index = 0;
+    	int index = 0, tempIndx=0;
     	while (iter.hasNext()) {
     		
-    		if (index > 0 ) {
-    			index = arrA.indexOf(iter.next());
-    		}
+    		tempIndx = arrA.indexOf(iter.next());
+    		if (tempIndx > index ) index = tempIndx;
     	}
-    	
     	
     	return index;
     }
